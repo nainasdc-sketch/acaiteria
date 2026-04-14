4920cb3d981d5dec4e86099d9f6585b401ab92cf
nainasdc-sketch <nainasdc@gmail.com>
2026-04-14 15:00:42 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,2 +1,237 @@
-<?php
-header('Location: listaFranquia.php');
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <!-- Codificação de caracteres para formato que aceite caracteres especiais como "ç é ã" -->
+    <meta charset="UTF-8">
+    <!-- Configurando para se moldar a diferentes tipos de telas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Título que aparece na barra do navegador -->
+    <title>O Melhor Açaí da Região Está Aqui no Amor de Açaí</title>
+
+    <!-- Dados estruturados para compartilhamento social -->
+    <meta property="og:locale" content="pt_BR">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="Açaí de dar Água na Boca">
+    <meta property="og:description" content="O melhor açaí da região centro-oeste brasileira. Aproveite o calor para se deliciar.">
+    <meta property="og:url" content="">
+    <meta property="og:site_name" content="Açai centro-oeste" />
+    <meta property="og:image" content="assets/imagens/logomarca.jpg" />
+
+    <!-- Inserindo arquivos de estilização -->
+    <link rel="stylesheet" href="assets/estilos/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/estilos/style.css">
+</head>
+<body>
+    <header class="main">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-3 col-sm-3 col-xs-0"></div>
+                <div class="col-lg-9 col-md-9 col-sm-9 col-xs-12">
+                    <div class="content">
+                        <span>Conheça a Açaíteria</span>
+                        <h1>Amor de Açaí</h1>
+                        <p>Temos o melhor açaí, cupuaçu e milk shake da região. Conheça as opções de acompanhamento.</p>
+                        <div class="content_buttons">
+                            <a href="loja.php" class="btn">Ir Para a Loja</a>
+                            <a href="sobre.html" class="btn">Conheça a Amor de Açaí</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <section class="view_02">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="block_main">
+                        <div class="column_left">
+                            <img src="assets/imagens/tigela-acai.webp" alt="Tigela de Açaí">
+                        </div>
+                        <div class="column_right">
+                            <span class="up_title">Açaí incrivelmente cremoso</span>
+                            <h2>Qualidade e sabor incomparável!</h2>
+                            <p>Nossa história começou em 2023. Com qualidade de proudução e sabor inigualável, conseguimos uma clientela fiel, que juntos têm feito a diferênça para nos tornarmos uma grande açaíteria. Conheça um pouco mais do Amor de Açaí.</p>
+                            <div class="buttons">
+                                <a href="sobre.html" class="btn">Mais Sobre a Açaíteria <i class="ic arrow"></i></a>
+                                <a href="https://api.whatsapp.com/send?phone=5561986521589&text=Ol%C3%A1%2C%20Gostaria%20de%20fazer%20um%20pedido." class="btn" target="_blank">Conversar por WhatsApp <i class="ic arrow"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                    <div class="block">
+                        <a href="produtos.html#acai">
+                            <img src="assets/imagens/acai.webp" width="300" height="300" alt="Fruto Açaí">
+                            <p class="title">Açaí</p>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                    <div class="block">
+                        <a href="produtos.html#cupuacu">
+                            <img src="assets/imagens/cupuacu.webp" width="300" height="300" alt="Cupuaçu">
+                            <p class="title">Cupuaçu</p>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                    <div class="block">
+                        <a href="produtos.html#milkshake">
+                            <img src="assets/imagens/milkshake.webp" width="300" height="300" alt="Milkshake">
+                            <p class="title">Mikshake</p>
+                        </a>
+                    </div>
+                </div>                
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                    <div class="block">
+                        <a href="produtos.html">
+                            <img src="assets/imagens/todos_os_produtos.webp" width="300" height="300" alt="Todos os nossos produtos">
+                            <p class="title">Todos os Produtos</p>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="view_03">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <h2 class="title">Acompanhe Nosso Podcast</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 background_podcast">
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                    <h2 class="subtitle">PODCAST EMPREENDEDOR #3</h2>
+                    <div class="block_video">
+                        <video width="560" height="315" controls>
+                            <source src="assets/videos/podcast-03.mp4" type="video/mp4">
+                            Seu navegador não suporta o elemento de vídeo.
+                        </video>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="view_04">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                    <div class="block_video">
+                        <video width="640" height="360" controls>
+                            <source src="assets/videos/processo-de-fabricacao-acai.mp4" type="video/mp4">
+                            Seu navegador não suporta o elemento de vídeo.
+                        </video>
+                        <p class="legend">Açaí selecionado, higienizado e bem acondicionado.</p>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                    <h2 class="title">O MELHOR PARA VOCÊ</h2>
+                    <ul>
+                        <li>Açaí<br>Selecionado</li>
+                        <li>Açaí<br>Higienizado</li>
+                        <li>Açaí<br>Original</li>
+                        <li>Açaí<br>Cremoso</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <?php include_once("_resources/formulario.php"); ?>
+
+    <footer>
+        <p class="copyright">©2023 Amor de Açaí - Todos os Direitos Reservados.</p>
+        
+        <!-- Adicione esta seção para o áudio -->
+        <div class="button_float" id="box_btn">
+            <button class="btn_musica" id="btn_play" onclick="toggleAudio()"></button>
+            <audio id="audio" controls style="display: none;">
+                <source src="assets/audio/musica_01.mp3" type="audio/mp3">
+                Seu navegador não suporta o elemento de áudio.
+            </audio>
+        </div>
+    </footer>
+
+    <!-- ADICIONANDO SCRIPTS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="assets/scripts/scripts.js"></script>
+    <script>
+        // Função para tocar ou pausar o áudio
+        function toggleAudio() {
+            var audio = document.getElementById("audio");
+            var btn = document.getElementById("btn_play");
+            var box_btn = document.getElementById("box_btn");
+
+            if (audio.paused) {
+                audio.play();
+                btn.style.backgroundImage = "url(assets/icons/ic_pause.webp)";
+            } else {
+                audio.pause();
+                btn.style.backgroundImage = "url(assets/icons/ic_musica.webp)";
+            }
+
+            // Adiciona a classe 'clicked' ao botão
+            box_btn.classList.toggle("clicked");
+        }
+    /* 
+        function validarFormulario() {
+            var nome = document.getElementById('nome').value;
+            var sobrenome = document.getElementById('sobrenome').value;
+            var telefone = document.getElementById('telefone').value;
+            var email = document.getElementById('email').value;
+
+            // Expressão regular para validar o formato do telefone (Brasil)
+            var regexTelefone = /^\(\d{2}\) \d{4,5}-\d{4}$/;
+
+            // Expressão regular para validar o formato do e-mail
+            var regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+            if (nome === '' || sobrenome === '' || telefone === '' || email === '') {
+                alert('Por favor, preencha todos os campos.');
+            } else if (!regexTelefone.test(telefone)) {
+                alert('Por favor, insira um número de telefone válido no formato (99) 99999-9999.');
+            } else if (!regexEmail.test(email)) {
+                alert('Por favor, insira um endereço de e-mail válido.');
+            } else {
+                // Aqui você pode adicionar código adicional para enviar os dados do formulário.
+                alert('Formulário enviado com sucesso!');
+                
+                // Recarregar a página após um pequeno atraso (1000 milissegundos = 1 segundo)
+                setTimeout(function() {
+                    location.reload();
+                }, 1000);
+            }
+        } */
+
+        // Função para formatar o número de telefone
+        function formatarTelefone(input) {
+            // Remove caracteres não numéricos
+            var num = input.value.replace(/\D/g, '');
+            
+            // Adiciona a máscara "(99) 99999-9999" ao número
+            var formattedNum;
+
+            if (num.length === 11) {
+                formattedNum = '(' + num.substring(0, 2) + ') ' + num.substring(2, 7) + '-' + num.substring(7, 11);
+            } else {
+                formattedNum = '(' + num.substring(0, 2) + ') ' + num.substring(2, 6) + '-' + num.substring(6, 10);
+            }
+            
+            // Atualiza o valor do input
+            input.value = formattedNum;
+        }
+      </script>
+</body>
+</html>
